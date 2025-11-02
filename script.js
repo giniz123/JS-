48015986fc28d41537b0889ce5ad04afd292fdcc
giniz123 <giniz.osti@gmail.com>
2025-11-03 10:11:32 +1100

    JS array method and traditional for loop

---
+++ script.js
@@ -0,0 +1,59 @@
+// array
+
+// let fruit = "apple";
+// const fruits = ["apple", "banana", "orange", "pear"];
+
+// console.log(fruits[3]);
+// fruits[1] = "mango";
+// console.log(fruits);
+
+// console.log(fruits.toString());
+
+// // console.log(fruits.length);
+
+// const length = fruits.length;
+// const result = fruits[length];
+
+// console.log(length);
+// console.log(result);
+
+// console.log(fruits[fruits.length - 1]);
+
+// console.log(typeof fruits);
+
+// console.log(Array.isArray(fruits));
+
+// For Loops
+
+// printing hello world 10 times
+const world = "Hello World ";
+
+for (i = 0; i < 10; i++) {
+  console.log(world, i);
+}
+
+// initiating multiple expression in for loop
+
+for (i = 0, j = 9; i < 10, j > 0; i++, j--) {
+  console.log(i, j);
+  console.log(`${i}${j}`);
+}
+
+// const fruits = ["apple", "banana", "orange", "mango", "pear"];
+// const vegetabkes = ["cauli", "brocoli", "peas", "cabbage", "potato"];
+// const meat = ["chicken", "pork", "goat", "beef", "buff"];
+
+// console.log(fruits.pop());
+// console.log(fruits);
+
+// const removedItem = fruits.shift();
+// console.log(removedItem);
+// console.log(fruits);
+
+// const newItem = fruits.unshift("grapes");
+// console.log(fruits);
+
+// const fruitAndVegies = fruits.concat(vegetabkes);
+// const fruitVegeMeat = fruits.concat(vegetabkes, meat);
+// console.log(fruitAndVegies);
+// console.log(fruitVegeMeat);
